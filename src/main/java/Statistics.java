992bc632d921caff4f17cbe9ff35cae93fb8b091
tobezznaczenia <50057081o@gmail.com>
2020-12-02 19:38:59 +0100

    Added methods for getting tasks and logged time ordered by dates

--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -1,3 +1,5 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Statistics {
@@ -104,5 +106,131 @@ public class Statistics {
         }
         return distinctTasksId;
     }
+
+    public Map<Long, Map<String, String>> getTasksAndDates(){
+        Map<Long, List<String>> usersAndTasks = getUsersAndTasks();
+        Map<Long, Map<String, String>> tasksWithDates = new HashMap<>();
+        SimpleDateFormat sf = new SimpleDateFormat("H.mm");
+        sf.setTimeZone(TimeZone.getTimeZone("GMT+0"));
+
+        for (Map.Entry<Long, List<String>> entry : usersAndTasks.entrySet()) {
+            Map<String, Long> datesWithHours = new HashMap<>();
+            Map<String, String> tasksWithHours = new HashMap<>();
+
+            for(String author : entry.getValue()){
+                workLogs.stream().filter(w->(w.author==author && w.taskId==entry.getKey())).forEach(w->{
+                    getDatesAndHours(w.date, w.timeLogged, datesWithHours);
+                });
+            }
+
+            for (Map.Entry<String, Long> dateWithHour : datesWithHours.entrySet()){
+                tasksWithHours.put(dateWithHour.getKey(), String.valueOf(sf.format(new Date(dateWithHour.getValue()))+"h"));
+            }
+            tasksWithDates.put(entry.getKey(), tasksWithHours);
+        }
+        return tasksWithDates;
+    }
+
+    public Map<Long, List<String>> getUsersAndTasks(){
+        Map<Long, List<String>> usersAndTasks = new HashMap<Long, List<String>>();
+        Set<Long> distinctTasksId = getDistinctTasks();
+        for(Long task : distinctTasksId){
+            List<String> users = new ArrayList<String>();
+            workLogs.stream().filter(w -> w.taskId==task).forEach(w->users.add(w.author));
+            usersAndTasks.put(task, users);
+        }
+        return usersAndTasks;
+    }
+
+    public Set<Long> getDistinctTasks(){
+        Set<Long> distinctTasksId = new HashSet<>();
+        for(Task task : taskList){
+            distinctTasksId.add(task.id);
+        }
+        return distinctTasksId;
+    }
+
+    public void getDatesAndHours(Long timestamp, Long time, Map<String, Long> datesWithHours){
+        Long timeInMilliseconds = time*3600000;
+
+        long timeSpent = (long)0;
+        String totalTimeSpent = "";
+
+        Date startHour = null;
+        Date endHour = null;
+
+        Date date = new Date(timestamp);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        Date dateAfterTimeSpend = new Date(timestamp+timeInMilliseconds);
+        Calendar calendarAfterTimeSpend = Calendar.getInstance();
+        calendarAfterTimeSpend.setTime(dateAfterTimeSpend);
+
+        Calendar calendarNextDay = Calendar.getInstance();
+        calendarNextDay.setTime(date);
+        calendarNextDay.add(Calendar.DATE, 1);
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+
+        try {
+            startHour = format.parse(calendar.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if(calendarAfterTimeSpend.get(Calendar.DAY_OF_YEAR) == calendarNextDay.get(Calendar.DAY_OF_YEAR)
+                && calendarAfterTimeSpend.get(Calendar.YEAR) == calendarNextDay.get(Calendar.YEAR)){
+            try {
+                endHour = format.parse("23:59");
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            ;
+            if(datesWithHours.containsKey(dateToString(date))){
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date), datesWithHours.get(dateToString(date))+timeSpent);
+            }else{
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date), timeSpent);
+            }
+            try {
+                startHour = format.parse("00:00");
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            try {
+                endHour = format.parse(calendarAfterTimeSpend.get(Calendar.HOUR_OF_DAY)+":"+calendar.get(Calendar.MINUTE));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if(datesWithHours.containsKey(dateToString(dateAfterTimeSpend))){
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date), datesWithHours.get(dateToString(date))+timeSpent);
+            }else{
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date), timeSpent);
+            }
+        }else{
+            try {
+                endHour = format.parse(calendarAfterTimeSpend.get(Calendar.HOUR_OF_DAY)+":"+calendarAfterTimeSpend.get(Calendar.MINUTE));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            if(datesWithHours.containsKey(dateToString(date))){
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date),datesWithHours.get(dateToString(date))+timeSpent);
+            }else{
+                timeSpent = endHour.getTime()-startHour.getTime();
+                datesWithHours.put(dateToString(date), timeSpent);
+            }
+        }
+    }
+
+    public String dateToString(Date date){
+        SimpleDateFormat sf = new SimpleDateFormat("yyyy-MM-dd");
+        return sf.format(date);
+    }
+
 
 }
